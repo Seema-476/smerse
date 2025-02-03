818f200a109eabc9da5d0a156b7c547f1883adff
Seema-476 <simmybishnoi29085@gmail.com>
2025-02-03 16:50:47 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 
-export const metadata = {
-  title: "Smerse",
-  description: "SMERSE FOR A WHEALTHIER LIFESTYLE. ANYTIME, ANYWHERE",
-  metadataBase: new URL('https://smerse-phi.vercel.app/'),
-  openGraph: {
-    images: '/meta-image.png',
-  },
-};
-
 export default function Home() {
   return (
     <>
